d7eda59bc6e301954d1242f27eaf2f4dcc125ce1
carlitosilva <carlito.silva@gmail.com>
2024-07-09 08:53:44 -0300

    criacao da migration de insert

---
+++ src/main/resources/db/migrations/V1__insert-into-usuario_sistema.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuario_sistema(chave_acesso,login) VALUES('PJE1G','PJE1G');
+INSERT INTO usuario_sistema(chave_acesso,login) VALUES('PJE2G','PJE2G');
+INSERT INTO usuario_sistema(chave_acesso,login) VALUES('PROJUDI','PROJUDI');
